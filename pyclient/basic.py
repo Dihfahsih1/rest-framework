eb406eff5ddf215d5f0e9158572a88a0f12f133e
Am Pythonista <dihfahsihm@gmail.com>
2022-09-11 01:40:04 +0300

    test rest framework on other endpoints

--- pyclient/basic.py
+++ pyclient/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint="http://127.0.0.1:8000/api/"
-response=requests.post(endpoint, json={"title":"product three", 'price':"500.07"})
+endpoint="http://127.0.0.1:8000/save_sub_category"
+response=requests.post(endpoint, json={"category":1,"name":"one"})
  
 print(response.json()) #body of the response returned 
 '''Normal http requests return html but REST API HTPP Request returns JSON or xml'''
