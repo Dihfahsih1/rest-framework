85991620acf448d96135ca7a41bad61964a08bf9
Am Pythonista <dihfahsihm@gmail.com>
2022-09-10 15:36:46 +0300

    create serializer for  for adding more functions and properties of a model

--- api/views.py
+++ api/views.py
@@ -6,14 +6,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import  json
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
-  model_data = Product.objects.order_by("?").first()
+  instance = Product.objects.order_by("?").first()
   data={}
-  if model_data:
-    data=model_to_dict(model_data, fields={'id','title','price'}) #specifiy the fields to retrieve
+  if instance:
+    data=model_to_dict(instance, fields={'id','title','price'}) #specifiy the fields to retrieve
     #data=model_to_dict(model_data)#get all fields
-       
+    data=ProductSerializer(instance).data
   return Response(data)
   
--- products/models.py
+++ products/models.py
@@ -4,3 +4,10 @@ class Product(models.Model):
   title = models.CharField(max_length=120,null=True,blank=True)
   content = models.CharField(max_length=120,null=True,blank=True)
   price = models.DecimalField(max_digits=120,null=True,blank=True, decimal_places=2, default=0.00)
+  
+  @property
+  def sale_price(self):
+    return "%.2f" %(float(self.price)*0.05)
+  
+  def get_discount(self):
+    return "50"
