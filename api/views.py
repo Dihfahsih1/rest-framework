2efc60cbb843befbe86f502f2ad4a03887fc36f9
Am Pythonista <dihfahsihm@gmail.com>
2022-09-10 14:50:55 +0300

    move from hard coded django way of  using api into use of rest-framework

--- api/views.py
+++ api/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+#from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import  json
 from products.models import Product
+
+@api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
   model_data = Product.objects.order_by("?").first()
   data={}
   if model_data:
-    data=model_to_dict(model_data, fields={'id','title','price'})
-    # data['Product ID']=model_data.id
-    # data['title']=model_data.title
-    # data['content']=model_data.content
-    # data['price']=model_data.price
+    data=model_to_dict(model_data, fields={'id','title','price'}) #specifiy the fields to retrieve
+    #data=model_to_dict(model_data)#get all fields
        
-  return JsonResponse(data)
+  return Response(data)
   
