e6f35e2fea34dab9c4b2c8b3496b8e9cdb94586d
Dihfahsih Mugoya <dihfahsihm@gmail.com>
2022-09-15 23:26:38 +0300

    add products details endpoint

--- api/views.py
+++ api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
 from django.forms.models import model_to_dict
 #from django.http import JsonResponse
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 import  json
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 @api_view(["POST"])
 def api_home(request, *args, **kwargs):
@@ -15,5 +15,5 @@ def api_home(request, *args, **kwargs):
     # instance=serializer.save()
     print(serializer.data)
     return Response(serializer.data)
-  return Response({"Invalid data"}, status=400)
+  return Response({"Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
   
--- pyclient/detail.py
+++ pyclient/detail.py
@@ -1,5 +1,6 @@
-"""Consuming the API"""
+"""COnsuming the API"""
 import requests
+import basic
 
 END_POINT="http://127.0.0.1:8000/save_sub_category"
 response=requests.post(END_POINT, json={"category":1,"name":"one"})
