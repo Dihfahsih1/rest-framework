e1a26df129080431ee1b3952568d2b7d46d0a0c2
dihsire <dihfahsihm@gmail.com>
2022-09-17 20:41:15 +0300

    replace files

--- api/views.py
+++ api/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
-from django.forms.models import model_to_dict
+"""Api Views"""
+#from django.shortcuts import render
+#from django.forms.models import model_to_dict
 #from django.http import JsonResponse
-import  json
+#import  json
 from products.models import Product
 from products.serializers import ProductSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 @api_view(["POST"])
-def api_home(request, *args, **kwargs):
-  serializer=ProductSerializer(data=request.data)
-  if serializer.is_valid(raise_exception=True):
-    # instance=serializer.save()
-    print(serializer.data)
-    return Response(serializer.data)
-  return Response({"Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
-  
+def api_home(request):
+    serializer=ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance=serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
+    
--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class PoductListCreateAPIView(generics.ListCreateAPIView):
+  queryset=Product.objects.all()
+  serializer_class=ProductSerializer
+  
+  def perform_create(self, serializer):
+    print(serializer)
+    serializer.save()
+    title=serializer.validated_data.get('title')
+    content=serializer.validated_data.get('content') or None
+    
+    if content is None:
+      content=title
+    serializer.save(content=content)
+  
+class ProductDetailAPIView(generics.RetrieveAPIView):
+  queryset=Product.objects.all()
+  serializer_class=ProductSerializer
 
 # Create your views here.
+
+def product_details(request, pk):
+  qs=Product.objects.get(id=pk)
+  print(qs.json())
+  context={"qs":qs}
+  return render(request, "details.html", context)
