798fd1ca78b4454299cb693dfaf3f8ca95acf7cf
cabap <dihfahsihm@gmail.com>
2022-09-18 21:04:51 +0300

    fix the bug of using the generic view

--- pyclient/update.py
+++ pyclient/update.py
@@ -4,7 +4,7 @@ import requests
 END_POINT="http://127.0.0.1:8000/api/products/1/update/"
 data = {
   "title":"Potatoes",
-  "price":2000
+  "price":2000,
 }
 response=requests.put(END_POINT, json=data)
 
