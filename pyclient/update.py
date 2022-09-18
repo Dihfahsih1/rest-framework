0a4b63433a9b79fcf42a7cb8fb448aaaafaeeddf
cabap <dihfahsihm@gmail.com>
2022-09-18 20:57:50 +0300

    add more functionalities

---
+++ pyclient/update.py
@@ -0,0 +1,12 @@
+"""update the product details"""
+import requests
+
+END_POINT="http://127.0.0.1:8000/api/products/1/update/"
+data = {
+  "title":"Potatoes",
+  "price":2000
+}
+response=requests.put(END_POINT, json=data)
+
+print(response.json())
+
