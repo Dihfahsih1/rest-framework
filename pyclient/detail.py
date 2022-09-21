1641db6e6ceb027390ee997894db639330f24797
ntcababp <dihfahsihm@gmail.com>
2022-09-21 20:50:58 +0300

    use mixins

--- pyclient/detail.py
+++ pyclient/detail.py
@@ -1,7 +1,7 @@
 """Fetching the products details"""
 import requests
 
-END_POINT="http://127.0.0.1:8000/api/products/4/details"
+END_POINT="http://127.0.0.1:8000/api/products/6/details"
 response=requests.get(END_POINT)
 
 print(response.json())
--- pyclient/list.py
+++ pyclient/list.py
@@ -1,7 +1,7 @@
 """Fetching all the created products"""
 import requests
 
-END_POINT="http://127.0.0.1:8000/api/products/"
+END_POINT="http://127.0.0.1:8000/api/products/list"
 response=requests.get(END_POINT)
 
 print(response.json())
