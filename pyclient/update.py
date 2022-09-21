d6ae2951d28cb7a4e3ee3ac8e7aad2e83a4237ce
ntcababp <dihfahsihm@gmail.com>
2022-09-21 18:57:11 +0300

    fix error causing the update to fail

--- pyclient/update.py
+++ pyclient/update.py
@@ -1,11 +1,13 @@
 """update the product details"""
 import requests
 
-END_POINT="http://127.0.0.1:8000/api/products/1/update/"
+END_POINT="http://127.0.0.1:8000/api/products/3/"
+
 data = {
-  "title":"Potatoes",
-  "price":2000,
+  "title": "Pizza",
+  "price": 2000,
 }
+
 response=requests.put(END_POINT, json=data)
 
 print(response.json())
