5fb7589c4e0e12a6ccb24d48724676dbd922392b
ntcababp <dihfahsihm@gmail.com>
2022-09-23 23:02:40 +0300

    Use authentication token to authorize access to the API

--- pyclient/list.py
+++ pyclient/list.py
@@ -3,8 +3,18 @@ import requests
 from getpass import getpass
 
 AUTH_END_POINT = "http://127.0.0.1:8000/api/auth/"
-PASSWORD = getpass()
-response=requests.post(AUTH_END_POINT, json={'username':'admin', 'password':PASSWORD})
+USERNAME = input("What is your username: \n")
+PASSWORD = getpass("Enter your password: \n")
+auth_response=requests.post(AUTH_END_POINT, json={'username':USERNAME, 'password':PASSWORD})
+print(auth_response.json())
 
-print(response.json())
+if auth_response.status_code == 200:
+  token = auth_response.json()['token']
+  headers = {
+    "Authorization": f"Token {token}"
+             }
+  END_POINT="http://127.0.0.1:8000/api/products/"
+  
+  get_response = requests.get(END_POINT, headers=headers)
+  print(get_response.json())
 
